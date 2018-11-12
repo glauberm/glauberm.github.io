8741e4a8cc6023716f9726ae564f1abe9abddac6
Glauber Mota <glaubernm@gmail.com>
2018-11-12 07:47:59 -0200

    Star Wars game

---
+++ star-wars-game/precache-manifest.95122a80655fbb9e028525b3180bd33c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ab4cea9e7cb69fb05df83ec291727d9",
+    "url": "/star-wars-game/static/media/imperial_emblem_front.6ab4cea9.svg"
+  },
+  {
+    "revision": "a9595b68f4fca57b2181",
+    "url": "/star-wars-game/static/css/main.e45425e1.chunk.css"
+  },
+  {
+    "revision": "fe05c2528a8acadc9380",
+    "url": "/star-wars-game/static/js/1.fe05c252.chunk.js"
+  },
+  {
+    "revision": "66c19485806ab59e2a3f",
+    "url": "/star-wars-game/static/js/runtime~main.66c19485.js"
+  },
+  {
+    "revision": "8e32644f272fa68de3713c3dc18d2120",
+    "url": "/star-wars-game/static/media/imperial_emblem_back.8e32644f.svg"
+  },
+  {
+    "revision": "3b14727c3425b8d73e4142144762d943",
+    "url": "/star-wars-game/static/media/imperial_emblem_back_choosable.3b14727c.svg"
+  },
+  {
+    "revision": "a9595b68f4fca57b2181",
+    "url": "/star-wars-game/static/js/main.a9595b68.chunk.js"
+  },
+  {
+    "revision": "fdbce9ecd64d97bb514a4a59c9d69635",
+    "url": "/star-wars-game/static/media/spin.fdbce9ec.svg"
+  },
+  {
+    "revision": "0f39a70e1e250cf2722f6d504b4e423d",
+    "url": "/star-wars-game/static/media/starjedi-webfont.0f39a70e.woff2"
+  },
+  {
+    "revision": "133825a6db5a5c229f6d997acd02e224",
+    "url": "/star-wars-game/static/media/starjedi-webfont.133825a6.woff"
+  },
+  {
+    "revision": "e56707e936f3817dcf2c5b8319cf9d7e",
+    "url": "/star-wars-game/static/media/RobotoSlab-Bold-webfont.e56707e9.woff"
+  },
+  {
+    "revision": "b52f7af97b4689c705c295fe3dde5128",
+    "url": "/star-wars-game/static/media/RobotoSlab-Regular-webfont.b52f7af9.woff"
+  },
+  {
+    "revision": "fdc07a7dbb75a1295ece441366e93c3a",
+    "url": "/star-wars-game/index.html"
+  }
+];
