a9ce78788830e38b71e21f993f5099c6007dd087
Glauber Mota <glaubernm@gmail.com>
2018-12-12 01:12:13 -0200

    Tweaks (2)

--- star-wars/precache-manifest.fcec7f223e3ac2e5f79921c229fefca1.js
+++ star-wars/precache-manifest.fcec7f223e3ac2e5f79921c229fefca1.js
@@ -1,50 +1,50 @@
 self.__precacheManifest = [
   {
     "revision": "fdbce9ecd64d97bb514a4a59c9d69635",
-    "url": "/star-wars-game/static/media/spin.fdbce9ec.svg"
+    "url": "/star-wars/static/media/spin.fdbce9ec.svg"
   },
   {
     "revision": "d20f554a328015bd82c6",
-    "url": "/star-wars-game/static/css/main.b311afe2.chunk.css"
+    "url": "/star-wars/static/css/main.b311afe2.chunk.css"
   },
   {
     "revision": "623669e90485fb5afca4",
-    "url": "/star-wars-game/static/js/1.623669e9.chunk.js"
+    "url": "/star-wars/static/js/1.623669e9.chunk.js"
   },
   {
     "revision": "66c19485806ab59e2a3f",
-    "url": "/star-wars-game/static/js/runtime~main.66c19485.js"
+    "url": "/star-wars/static/js/runtime~main.66c19485.js"
   },
   {
     "revision": "8e32644f272fa68de3713c3dc18d2120",
-    "url": "/star-wars-game/static/media/imperial_emblem_back.8e32644f.svg"
+    "url": "/star-wars/static/media/imperial_emblem_back.8e32644f.svg"
   },
   {
     "revision": "6ab4cea9e7cb69fb05df83ec291727d9",
-    "url": "/star-wars-game/static/media/imperial_emblem_front.6ab4cea9.svg"
+    "url": "/star-wars/static/media/imperial_emblem_front.6ab4cea9.svg"
   },
   {
     "revision": "d20f554a328015bd82c6",
-    "url": "/star-wars-game/static/js/main.d20f554a.chunk.js"
+    "url": "/star-wars/static/js/main.d20f554a.chunk.js"
   },
   {
     "revision": "0f39a70e1e250cf2722f6d504b4e423d",
-    "url": "/star-wars-game/static/media/starjedi-webfont.0f39a70e.woff2"
+    "url": "/star-wars/static/media/starjedi-webfont.0f39a70e.woff2"
   },
   {
     "revision": "e56707e936f3817dcf2c5b8319cf9d7e",
-    "url": "/star-wars-game/static/media/RobotoSlab-Bold-webfont.e56707e9.woff"
+    "url": "/star-wars/static/media/RobotoSlab-Bold-webfont.e56707e9.woff"
   },
   {
     "revision": "133825a6db5a5c229f6d997acd02e224",
-    "url": "/star-wars-game/static/media/starjedi-webfont.133825a6.woff"
+    "url": "/star-wars/static/media/starjedi-webfont.133825a6.woff"
   },
   {
     "revision": "b52f7af97b4689c705c295fe3dde5128",
-    "url": "/star-wars-game/static/media/RobotoSlab-Regular-webfont.b52f7af9.woff"
+    "url": "/star-wars/static/media/RobotoSlab-Regular-webfont.b52f7af9.woff"
   },
   {
     "revision": "ee9c9d81f1e6e5a1336938597cd7239a",
-    "url": "/star-wars-game/index.html"
+    "url": "/star-wars/index.html"
   }
 ];
